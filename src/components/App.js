cf160b432e6306dbd32b75196fcac89da59e1a1f
Vishnu tyagi <vishnu.tyagi.mec20@itbhu.ac.in>
2024-08-08 14:20:34 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,45 @@
-
-import React from "react";
+import React, { useEffect } from "react";
 import './../styles/App.css';
+// import axios from "axios";
+import { useState } from "react";
+import 'regenerator-runtime/runtime';
+import 'text-encoding';
+
+
+const api = "https://dummyjson.com/products";
 
 const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(api);
+        setData(JSON.stringify(response)); 
+        console.log(JSON.stringify(response));
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div>
-        {/* Do not remove the main div */}
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          <h1>Data Fetched from API</h1>
+          <pre>{JSON.stringify(data)}</pre>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
